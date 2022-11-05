e9f4567c60f5dc5956cada6b69f71b858b0feba6
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-11-05 22:04:35 +0530

    home navbard added

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (<>
         <div>Home </div>
-        <h2 className="text-xl font-bold text-red-600  max-w-4xl ">hello home beta ....🥂⚫🟢</h2>
+        <h2 className="text-xl font-bold text-red-600  ">hello home beta ....🥂⚫🟢</h2>
     </>
     )
 }
